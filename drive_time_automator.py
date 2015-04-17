7a80c09b360a248f3ac5d11423fcf6e1c8660a08
JeffreyJackovich <JeffreyJackovich@users.noreply.github.com>
2015-04-17 15:35:30 -0400

    Create drive_time_automator.py

---
+++ drive_time_automator.py
@@ -0,0 +1,18 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import FirefoxProfile
+import user_agents
+
+
+
+import re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.proxy import *
+import pandas as pd
+import datetime
+import time
+import random
+from selenium import webdriver, selenium
+
